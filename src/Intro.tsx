6a81d019af4575d9e89884d5d69b45ae8a0ced57
JT <jajcek.30@gmail.com>
2022-10-15 22:24:08 +0200

    finished intro page

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import {Link} from "react-router-dom";
 
 const Container = styled.div`
     background-color: #f4f4f4;
@@ -16,11 +17,43 @@ const Centered = styled.div`
 `;
 
 const Name = styled.div`
-
+    letter-spacing: 20px;
+    font-size: 45px;
+    font-weight: 700;
 `;
 
 const Profesion = styled.div`
+    letter-spacing: 8px;
+    font-size: 13px;
+    font-weight: 700;
+`;
 
+const fontColor = '#f4f4f4';
+
+const Nav = styled.nav`
+    display: flex;
+    margin-top: 2%;
+    gap: 10px;
+`;
+
+const ButtonLink = styled(Link)`
+    background-color: #383838;
+    border: 4px solid #ccc;
+    transition: background-color .5s ease-in-out;
+    width: 100px;
+    text-align: center;
+    padding: 5px;
+    font-size: 14px;
+
+    &, &:visited {
+        color: ${fontColor};
+        text-decoration: none;
+    }
+
+    &:hover {
+        background-color: #686868;
+        transition: background-color 0s ease-in-out;
+    }
 `;
 
 class Intro extends React.Component<{}, {}> {
@@ -29,6 +62,11 @@ class Intro extends React.Component<{}, {}> {
             <Centered>
                 <Name>JACEK TOPOLSKI</Name>
                 <Profesion>SOFTWARE DEVELOPER</Profesion>
+                <Nav>
+                    <ButtonLink to={'/travel'}>About</ButtonLink>
+                    <ButtonLink to={'/work'}>Work</ButtonLink>
+                    <ButtonLink to={'/contact'}>Contact</ButtonLink>
+                </Nav>
             </Centered>
         </Container>;
     }
