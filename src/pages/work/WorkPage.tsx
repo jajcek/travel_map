64fd10164bf3ab9e018036066dd9a66b541727a5
JT <jajcek.30@gmail.com>
2022-10-30 23:37:33 +0100

    fixed displaying items when there are more than one, added new item

--- src/pages/work/WorkPage.tsx
+++ src/pages/work/WorkPage.tsx
@@ -19,6 +19,16 @@ const Centered = styled.div`
     text-align: center;
 `;
 
+const ItemsContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 50px;
+
+    > div {
+        margin: 5px;
+    }
+`;
+
 class WorkPage extends React.Component<{}, {}> {
     render() {
         return (
@@ -26,7 +36,16 @@ class WorkPage extends React.Component<{}, {}> {
                 <Centered>
                     <h2>Software development</h2>
                     <p>Below you can find some programs I have created. Smaller and bigger.</p>
-                    <WorkItem image="work/infinite_terrain/thumbnail.png" name="Infinite 3D terrain" tech="C++/DirectX 11/HLSL" />
+                    <ItemsContainer>
+                        <WorkItem image="work/infinite_terrain/thumbnail.png" name="Infinite 3D terrain" tech="C++/DirectX 11/HLSL" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                        <WorkItem image="work/do_for_me/thumbnail.png" name="DoForMe!" tech="C++/Qt/Lua" />
+                    </ItemsContainer>
 
                     <h2>3D graphics</h2>
                     <p>Some years ago I was also playing around with 3D graphics. Below you can find some of my projects.</p>
